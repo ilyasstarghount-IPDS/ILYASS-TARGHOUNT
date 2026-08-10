1f2c49e6199f5815d21808a807cdbe5f9a7c83cb
Ilyass Targhount <ilyass.targhount@edu.uiz.ac.ma>
2026-08-10 16:17:16 +0100

    Fix final routing and auto base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- HADA HUA L-MOUHIM (Relative path)
+  // Netlify kay-dir build kay-ya3tih '/', f GH Pages kay-ya3tih '/ILYASS-TARGHOUNT/'
+  base: process.env.NETLIFY ? '/' : '/ILYASS-TARGHOUNT/',
 })
