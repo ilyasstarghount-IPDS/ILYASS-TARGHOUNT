d851b7fbfa8f9f5797f7f743beb6cc8c11ea0ce9
Ilyass Targhount <ilyass.targhount@edu.uiz.ac.ma>
2026-08-10 12:48:55 +0100

    Fix deployment for Netlify and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ILYASS-TARGHOUNT/',
+  base: process.env.GITHUB_PAGES === 'true'
+    ? '/ILYASS-TARGHOUNT/'
+    : '/',
 })
