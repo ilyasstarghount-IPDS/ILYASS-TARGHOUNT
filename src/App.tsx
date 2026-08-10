217ff7ee2d3c2ce70a2deaaa63ea125ea79a52c5
Ilyass Targhount <ilyass.targhount@edu.uiz.ac.ma>
2026-08-10 15:33:25 +0100

    Fix base path with relative assets

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Droites from "./pages/Droites";
    SCROLL TO TOP
 ========================= */
 
-function ScrollToTop() {
+function ScrollToTop(): null {
   const { pathname } = useLocation();
 
   useEffect(() => {
