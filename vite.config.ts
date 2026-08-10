24d65fdf996fb61cb73ba60969f6647a5782b30f
Ilyass Targhount <ilyass.targhount@edu.uiz.ac.ma>
2026-08-10 13:38:10 +0100

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true'
+  base: mode === 'github'
     ? '/ILYASS-TARGHOUNT/'
     : '/',
-})
+}))
